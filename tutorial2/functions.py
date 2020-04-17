7e71d5e1390f0c53df5baf40d113fd6e96042509
Jordan <j.wiltshire1@uni.brighton.ac.uk>
2020-04-17 13:34:19 +0100

    added tutorial 2

---
+++ tutorial2/functions.py
@@ -0,0 +1,19 @@
+def addition(list):
+	i = 0
+	for numbers in list:
+		i += numbers
+	print (i)
+
+
+def multiply(list):
+	i = 1
+	for numbers in list:
+		i = i*numbers
+	print(i)
+
+def conc(list):
+	sentance = ""
+	for words in list:
+		sentance = sentance + " " + words
+	print(sentance) 
+

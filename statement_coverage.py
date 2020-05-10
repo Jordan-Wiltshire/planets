fada0ad49d3d3a6ae1cfbe64193a86bbe0076645
Jordan <j.wiltshire1@uni.brighton.ac.uk>
2020-05-10 23:53:08 +0100

    small program to perform statement coverage on.

---
+++ statement_coverage.py
@@ -0,0 +1,9 @@
+def printsum (int a, int b):
+	sum = a + b
+	if(sum > 0)
+		print("positive " + sum)
+	if(sum < 0)
+		print("negative " + sum)
+	if(sum == 0)
+		print("positive/negative " + sum)
+	return

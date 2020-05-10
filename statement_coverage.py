d4e343895767ce9a5246982cb100b4e0e700903a
Jordan <j.wiltshire1@uni.brighton.ac.uk>
2020-05-10 23:55:30 +0100

    fixed bugs in statement_coverage.py

--- statement_coverage.py
+++ statement_coverage.py
@@ -1,9 +1,11 @@
-def printsum (int a, int b):
+def printsum (a, b):
 	sum = a + b
-	if(sum > 0)
-		print("positive " + sum)
-	if(sum < 0)
-		print("negative " + sum)
-	if(sum == 0)
-		print("positive/negative " + sum)
+	if(sum > 0):
+		print("positive " + str(sum))
+	if(sum < 0):
+		print("negative " + str (sum))
+	if(sum == 0):
+		print("positive/negative " + str(sum))
 	return
+
+printsum(2, 2)

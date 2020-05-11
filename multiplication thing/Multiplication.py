eb92e9899b8d93fa53a692d57f8e9c88b6ad1d0e
Jordan <j.wiltshire1@uni.brighton.ac.uk>
2020-05-11 10:25:45 +0100

    changed operator symobols in Multiplication.py

--- multiplication thing/Multiplication.py
+++ multiplication thing/Multiplication.py
@@ -1,13 +1,14 @@
-def noStar(a, b):
-	if a > 100 and b > 100:
-		c = 0
-		d = 0
-		while d < a:
-			c = c + b
-			d += 1
-		return c
-	else:
-		print("two digit numbers only")
+class multiply:
+	def noStar(a, b):
+		if a < 100 and b < 100:
+			c = 0
+			d = 0
+			while d < a:
+				c = c + b
+				d += 1
+			return c
+		else:
+			print("two digit numbers only")
 
 num1 = int(input()) 
 num2 = int(input())

f99412023769aedd67453c72adc6ccf2b7013ff7
Jordan <j.wiltshire1@uni.brighton.ac.uk>
2020-05-11 02:09:16 +0100

    added automated testing to statement coverage

--- statement_coverage.py
+++ statement_coverage.py
@@ -1,11 +1,39 @@
-def printsum (a, b):
-	sum = a + b
-	if(sum > 0):
-		print("positive " + str(sum))
-	if(sum < 0):
-		print("negative " + str (sum))
-	if(sum == 0):
-		print("positive/negative " + str(sum))
-	return
+import unittest
 
-printsum(2, 2)
+class sum:
+	def printsum (self, a, b):
+		sum = a + b
+		if(sum > 0):
+			product = "positive " + str(sum)
+			return product
+		if(sum < 0):
+			product = "negative " + str (sum)
+			return product
+		if(sum == 0):
+			product = "positive/negative " + str(sum)
+			return product
+
+
+
+class printsumTest(unittest.TestCase):
+
+	def setUp(self):
+		self.myclass = sum()
+
+	def test_Negativeprintsum(self):
+
+		self.assertEqual(self.myclass.printsum(-4,2) , "negative -2")
+
+	def test_Positiveprintsum(self):
+
+		self.assertEqual(self.myclass.printsum(4,2) , "positive 6")
+
+	def test_Zeroprintsum(self):
+
+		self.assertEqual(self.myclass.printsum(-4,4) , "positive/negative 0")
+
+	def tearDown(self):
+		del self.myclass
+
+if __name__ == '__main__':
+    unittest.main()
